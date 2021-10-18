346ddd8edcd6840238bc9961ca5a978ba5557bd1
arif-pandu <mapanduprayoga@gmail.com>
2021-10-18 11:38:31 +0700

    Added Splash Screen, Rename, and maybe icon

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "NOTification",
       home: const HomePage(),
       // home: TesTextField(),
     );
